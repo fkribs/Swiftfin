6aa880886e1c1f9902d1421c6ef3df5d50e65220
Joe Kribs <joseph@kribs.net>
2023-05-29 20:09:07 -0600

    H Drawer Lettered Filter V1

--- Swiftfin/Components/FilterDrawerHStack/FilterDrawerHStack.swift
+++ Swiftfin/Components/FilterDrawerHStack/FilterDrawerHStack.swift
@@ -14,62 +14,91 @@ struct FilterDrawerHStack: View {
     @ObservedObject
     var viewModel: FilterViewModel
     private var onSelect: (FilterCoordinator.Parameters) -> Void
+    
+    let letters: [String] = (0..<26).map { index in
+        String(UnicodeScalar("A".unicodeScalars.first!.value + index)!)
+    }
 
     var body: some View {
-        HStack {
-            if viewModel.currentFilters.hasFilters {
-                Menu {
-                    Button(role: .destructive) {
-                        viewModel.currentFilters = .init()
+        VStack {
+            HStack(spacing: 4) {
+                if viewModel.currentFilters.hasFilters {
+                    Menu {
+                        Button(role: .destructive) {
+                            viewModel.currentFilters = .init()
+                        } label: {
+                            L10n.reset.text
+                        }
                     } label: {
-                        L10n.reset.text
+                        FilterDrawerButton(systemName: "line.3.horizontal.decrease.circle.fill", activated: true)
                     }
-                } label: {
-                    FilterDrawerButton(systemName: "line.3.horizontal.decrease.circle.fill", activated: true)
                 }
+                
+                FilterDrawerButton(title: L10n.genres, activated: viewModel.currentFilters.genres != [])
+                    .onSelect {
+                        onSelect(.init(
+                            title: L10n.genres,
+                            viewModel: viewModel,
+                            filter: \.genres,
+                            selectorType: .multi
+                        ))
+                    }
+                
+                FilterDrawerButton(title: L10n.filters, activated: viewModel.currentFilters.filters != [])
+                    .onSelect {
+                        onSelect(.init(
+                            title: L10n.filters,
+                            viewModel: viewModel,
+                            filter: \.filters,
+                            selectorType: .multi
+                        ))
+                    }
+                
+                // TODO: Localize
+                FilterDrawerButton(title: "Order", activated: viewModel.currentFilters.sortOrder != [APISortOrder.ascending.filter])
+                    .onSelect {
+                        onSelect(.init(
+                            title: "Order",
+                            viewModel: viewModel,
+                            filter: \.sortOrder,
+                            selectorType: .single
+                        ))
+                    }
+                
+                // TODO: Localize
+                FilterDrawerButton(title: "Sort", activated: viewModel.currentFilters.sortBy != [SortBy.name.filter])
+                    .onSelect {
+                        onSelect(.init(
+                            title: "Sort",
+                            viewModel: viewModel,
+                            filter: \.sortBy,
+                            selectorType: .single
+                        ))
+                    }
             }
-
-            FilterDrawerButton(title: L10n.genres, activated: viewModel.currentFilters.genres != [])
-                .onSelect {
-                    onSelect(.init(
-                        title: L10n.genres,
-                        viewModel: viewModel,
-                        filter: \.genres,
-                        selectorType: .multi
-                    ))
+            .padding(.horizontal, 4)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 8) {
+                    ForEach(letters, id: \.self) { letter in
+                        Button(action: {
+                            // TODO: Handle selection of letter
+                        }) {
+                            Text(letter)
+                                .font(.system(size: 12))
+                                .padding(.vertical, 4)
+                        }
+                        .frame(width: 30, height: 30)
+                        .background {
+                            Capsule()
+                                .foregroundColor(Color(UIColor.secondarySystemFill))
+                            //.foregroundColor(activated ? accentColor : Color(UIColor.secondarySystemFill))
+                                .opacity(0.5)
+                        }
+                        .cornerRadius(15)
+                    }
                 }
-
-            FilterDrawerButton(title: L10n.filters, activated: viewModel.currentFilters.filters != [])
-                .onSelect {
-                    onSelect(.init(
-                        title: L10n.filters,
-                        viewModel: viewModel,
-                        filter: \.filters,
-                        selectorType: .multi
-                    ))
-                }
-
-            // TODO: Localize
-            FilterDrawerButton(title: "Order", activated: viewModel.currentFilters.sortOrder != [APISortOrder.ascending.filter])
-                .onSelect {
-                    onSelect(.init(
-                        title: "Order",
-                        viewModel: viewModel,
-                        filter: \.sortOrder,
-                        selectorType: .single
-                    ))
-                }
-
-            // TODO: Localize
-            FilterDrawerButton(title: "Sort", activated: viewModel.currentFilters.sortBy != [SortBy.name.filter])
-                .onSelect {
-                    onSelect(.init(
-                        title: "Sort",
-                        viewModel: viewModel,
-                        filter: \.sortBy,
-                        selectorType: .single
-                    ))
-                }
+                .padding(.horizontal, 8)
+            }
         }
     }
 }
