245b1b640f8dc3e24ca8495b99e19e73f098fb9e
Joe Kribs <joseph@kribs.net>
2023-06-02 08:07:19 -0600

    Removing the original scrollbar

--- Swiftfin/Components/PagingLibraryView.swift
+++ Swiftfin/Components/PagingLibraryView.swift
@@ -12,17 +12,17 @@ import JellyfinAPI
 import SwiftUI
 
 struct PagingLibraryView: View {
-    
+
     @Default(.Customization.Library.gridPosterType)
     private var libraryGridPosterType
     @Default(.Customization.Library.viewType)
     private var libraryViewType
-    
+
     @ObservedObject
     var viewModel: PagingLibraryViewModel
-    
+
     private var onSelect: (BaseItemDto) -> Void
-    
+
     private var gridLayout: NSCollectionLayoutSection.GridLayoutMode {
         if libraryGridPosterType == .landscape && UIDevice.isPhone {
             return .fixedNumberOfColumns(2)
@@ -30,104 +30,84 @@ struct PagingLibraryView: View {
             return .adaptive(withMinItemSize: libraryGridPosterType.width + (UIDevice.isIPad ? 10 : 0))
         }
     }
-    
-    struct ScrollBarPreferenceKey: PreferenceKey {
-        static var defaultValue: CGRect = .zero
-        
-        static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
-            value = nextValue()
+
+    @ViewBuilder
+    private var libraryListView: some View {
+        CollectionView(items: viewModel.items.elements) { _, item, _ in
+            LibraryItemRow(item: item)
+                .onSelect {
+                    onSelect(item)
+                }
+                .padding()
+        }
+        .layout { _, layoutEnvironment in
+            .list(using: .init(appearance: .plain), layoutEnvironment: layoutEnvironment)
+        }
+        .willReachEdge(insets: .init(top: 0, leading: 0, bottom: 200, trailing: 0)) { edge in
+            if !viewModel.isLoading && edge == .bottom {
+                viewModel.requestNextPage()
+            }
+        }
+        .onEdgeReached { edge in
+            if viewModel.hasNextPage, !viewModel.isLoading, edge == .bottom {
+                viewModel.requestNextPage()
+            }
+        }
+        .configure { configuration in
+            configuration.showsVerticalScrollIndicator = false
         }
     }
-    
-    @State private var selectedLetter: String = ""
-    
+
+    @ViewBuilder
+    private var libraryGridView: some View {
+        CollectionView(items: viewModel.items.elements) { _, item, _ in
+            PosterButton(state: .item(item), type: libraryGridPosterType)
+                .scaleItem(libraryGridPosterType == .landscape && UIDevice.isPhone ? 0.85 : 1)
+                .onSelect {
+                    onSelect(item)
+                }
+        }
+        .layout { _, layoutEnvironment in
+            .grid(
+                layoutEnvironment: layoutEnvironment,
+                layoutMode: gridLayout,
+                sectionInsets: .init(top: 0, leading: 10, bottom: 0, trailing: 10)
+            )
+        }
+        .willReachEdge(insets: .init(top: 0, leading: 0, bottom: 200, trailing: 0)) { edge in
+            if !viewModel.isLoading && edge == .bottom {
+                viewModel.requestNextPage()
+            }
+        }
+        .onEdgeReached { edge in
+            if viewModel.hasNextPage, !viewModel.isLoading, edge == .bottom {
+                viewModel.requestNextPage()
+            }
+        }
+        .configure { configuration in
+            configuration.showsVerticalScrollIndicator = false
+        }
+    }
+
     var body: some View {
-        let letters: [String] = (0..<26).map { index in
-            String(UnicodeScalar("A".unicodeScalars.first!.value + index)!)
+        switch libraryViewType {
+        case .grid:
+            libraryGridView
+        case .list:
+            libraryListView
         }
-        
-        HStack(spacing: 0) {
-            CollectionView(items: viewModel.items.elements) { _, item, _ in
-                PosterButton(state: .item(item), type: libraryGridPosterType)
-                    .scaleItem(libraryGridPosterType == .landscape && UIDevice.isPhone ? 0.85 : 1)
-                    .onSelect {
-                        onSelect(item)
-                    }
-            }
-            .layout { _, layoutEnvironment in
-                .grid(
-                    layoutEnvironment: layoutEnvironment,
-                    layoutMode: gridLayout,
-                    sectionInsets: .init(top: 0, leading: 10, bottom: 0, trailing: 10)
-                )
-            }
-            .willReachEdge(insets: .init(top: 0, leading: 0, bottom: 200, trailing: 0)) { edge in
-                if !viewModel.isLoading && edge == .bottom {
-                    viewModel.requestNextPage()
-                }
-            }
-            .onEdgeReached { edge in
-                if viewModel.hasNextPage, !viewModel.isLoading, edge == .bottom {
-                    viewModel.requestNextPage()
-                }
-            }
-            
-            ScrollViewReader { scrollViewProxy in
-                ScrollView(.vertical) {
-                    VStack(spacing: 0) {
-                        ForEach(letters, id: \.self) { letter in
-                            Button(action: {
-                                withAnimation {
-                                    scrollViewProxy.scrollTo(letter, anchor: .top)
-                                }
-                            }) {
-                                Text(letter)
-                                    .font(.system(size: 12))
-                                    .padding(.vertical, 4)
-                            }
-                            .id(letter)
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .background(
-                    GeometryReader { proxy in
-                        Color.clear
-                            .preference(key: ScrollBarPreferenceKey.self, value: proxy.frame(in: .global))
-                    }
-                    .onPreferenceChange(ScrollBarPreferenceKey.self) { frame in
-                        let minY = frame.minY
-                        let maxY = frame.maxY
-                        let scrollHeight = maxY - minY
-                        let scrollPosition = minY + (scrollHeight / CGFloat(letters.count)) * CGFloat(letters.firstIndex(of: selectedLetter) ?? 0)
-                        let contentOffset = CGPoint(x: 0, y: scrollPosition)
-                    }
-                )
-            }
-            .onChange(of: selectedLetter) { newValue in
-                // Handle scroll to specific section
-                // Add your code to scroll to the desired section
-            }
-            .onAppear {
-                if let firstLetter = letters.first {
-                    selectedLetter = firstLetter
-                }
-            }
-        }
-        .introspectScrollView { scrollView in
-            scrollView.showsVerticalScrollIndicator = false
-        }
-    }
-    
-    init(viewModel: PagingLibraryViewModel) {
-        self.viewModel = viewModel
-        self.onSelect = { _ in }
-    }
-    
-    func onSelect(_ action: @escaping (BaseItemDto) -> Void) -> PagingLibraryView {
-        var updatedView = self
-        updatedView.onSelect = action
-        return updatedView
     }
 }
 
+extension PagingLibraryView {
+    init(viewModel: PagingLibraryViewModel) {
+        self.init(
+            viewModel: viewModel,
+            onSelect: { _ in }
+        )
+    }
+
+    func onSelect(_ action: @escaping (BaseItemDto) -> Void) -> Self {
+        copy(modifying: \.onSelect, with: action)
+    }
+}
